39c306897f06f8ebf979b9512512d3d28120ef86
Ignacy Pawlak <ignacy.pawlak@gmail.com>
2022-01-10 22:28:01 +0100

    Add project files.

---
+++ BookStoreApi/BookStoreApi/Model/DatabaseContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStoreApi.Model
+{
+    public class DatabaseContext:DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options):base(options)
+        {
+
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=bookStore.db");
+        }
+
+        public virtual DbSet<Book> Books { get; set; }
+    }
+}
